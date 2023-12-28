6698d78bdb479fbc9e05922ca321507a2d0f08de
김영택 <kooduck168@gmail.com>
2023-12-28 14:28:54 +0900

    Pull

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -3,7 +3,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { ChatsDocument } from './chatDocument/chats.document';
 import { ChatsType } from './chatDocument/chats.document';
-import { ChatsGateway } from './chats.gateway';
 import { MyDocument, MyType } from 'src/mongo/mongo.document';
 
 @Injectable()
